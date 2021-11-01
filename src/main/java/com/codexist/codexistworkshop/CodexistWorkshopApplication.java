46b3d027b5708608172d886e720e398c8a4db534
Muhammet Albayrak <mhasanalbyrk@gmail.com>
2021-11-01 17:27:25 +0300

    Initial commit

---
+++ src/main/java/com/codexist/codexistworkshop/CodexistWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.codexist.codexistworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodexistWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodexistWorkshopApplication.class, args);
+    }
+
+}
